ac2f2791128f73e34e636681ebc0937a50cf462c
Haroon210976 <shahssg1357@gmail.com>
2024-10-12 23:06:38 +0500

    login , logout routes and auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,26 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { log } from '../contants.js';
 
+// function : generate access and refresh tokens
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await User.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      'Issues while generating Access and Refresh Tokens'
+    );
+  }
+};
+
+// Register User
 const registerUser = asyncHandler(async (req, res) => {
   // Get data from the User
   // Validate Data
@@ -22,7 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // Data Validation
   if (
     [fullName, email, username, password].some(
-      (field) => field?.trim() === '' || field === undefined || field === null
+      (field) => field === undefined || field === null || field?.trim() === ''
     )
   ) {
     throw new ApiError(400, 'All Fields are Compulsaory');
@@ -80,4 +100,99 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, 'User Created Successfully'));
 });
 
-export { registerUser };
+// Login User
+const loginUser = asyncHandler(async (req, res) => {
+  // Get data from user
+  // validate the fields
+  // check for the username or email in db
+  // compare the password
+  // if (credentials == valid) {
+  //    generate access and refresh token and send it to client through cookies
+  // }else{
+  //    throw an error
+  // }
+
+  // Get data
+  const { username, email, password } = req.body;
+
+  // Validate fields
+  if (
+    [username || email, password].some(
+      (field) => field === undefined || field === null || field?.trim() === ''
+    )
+  ) {
+    throw new ApiError(404, 'All fields are Compulsory');
+  }
+
+  // check for username or email
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, 'User with username or email is not found.');
+  }
+
+  // Password check
+  const isvalidPassword = await user.isPasswordCorrect(password);
+
+  if (!isvalidPassword) {
+    throw new ApiError(404, 'Invalid Credientails');
+  }
+
+  // Generate Access and Refresh tokens
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    '-password -refreshToken'
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie('Access Token', accessToken, options)
+    .cookie('Refresh Token', refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        'User Successfully LoggedIn'
+      )
+    );
+});
+
+// Logout User
+const logoutUser = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1, // removes the field from the database
+      },
+    },
+    { new: true }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie('accessToken', options)
+    .clearCookie('refreshToken', options)
+    .json(new ApiResponse(200, {}, 'User Logout Successfully'));
+});
+
+export { registerUser, loginUser, logoutUser };
