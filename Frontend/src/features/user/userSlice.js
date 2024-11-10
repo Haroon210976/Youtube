6479efcc549f0fc14f24dd1eca245bd70abac29a
Haroon210976 <shahssg1357@gmail.com>
2024-11-10 11:19:14 +0500

    Front end setup

---
+++ Frontend/src/features/user/userSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
